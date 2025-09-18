2ba7b0274dec3fa204b3a61101300054411031b8
Matin Hassan Pour <matinhp04@gmail.com>
2025-09-18 14:04:57 -0700

    import homepage components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Navbar from "@/components/home/Navbar";
 import HeroSection from "@/components/home/Hero";
+import Carousel from "@/components/home/Carousel";
+import Locations from "@/components/home/Locations";
+import WatchOnline from "@/components/home/WatchOnline";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Carousel />
+      <Locations />
+      <WatchOnline />
     </>
   );
 }
