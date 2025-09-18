fd26bc2e11f3bf0ac721fa0549ee33277370c1cb
Matin Hassan Pour <matinhp04@gmail.com>
2025-09-18 01:21:48 -0700

    feat. locations section initial layout, image, links

---
+++ src/components/home/Locations.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Locations() {
+  return (
+    <section className="py-8">
+      <h2 className="font-semibold text-2xl text-center">Our Locations</h2>
+      <div className="flex justify-center align-center">
+        <Image
+          src="/home/canada-map.png"
+          alt="Locations"
+          className=""
+          width={700}
+          height={400}
+        />
+      </div>
+      <div className="flex justify-center align-center space-x-18 py-4 text-lg">
+        <a className="hover:underline transition " href="#">North Shore</a>
+        <a className="hover:underline transition " href="#">Squamish</a>
+        <a className="hover:underline transition " href="#">Toronto</a>
+      </div>
+    </section>
+  );
+}
