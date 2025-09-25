bd58dafbafca16fbbba2bdc961a76c9df29a3ab9
Matin Hassan Pour <matinhp04@gmail.com>
2025-09-25 10:35:56 -0700

    fix. button layout

--- src/components/home/Locations.tsx
+++ src/components/home/Locations.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Locations() {
   return (
     <section id="locations" className="py-8">
       <h2 className="font-semibold text-2xl text-center">Our Locations</h2>
-      <div className="flex justify-center align-center">
+      <div className="flex justify-center">
         <Image
           src="/home/canada-map.png"
           alt="Locations"
@@ -13,10 +14,10 @@ export default function Locations() {
           height={400}
         />
       </div>
-      <div className="flex justify-around align-center py-4 text-lg max-w-screen-2xl">
-        <a className="hover:underline transition" href="#">North Shore</a>
-        <a className="hover:underline transition" href="#">Squamish</a>
-        <a className="hover:underline transition" href="#">Toronto</a>
+      <div className="flex justify-center items-center py-4 text-lg gap-18 md:gap-36">
+        <Link className="hover:underline transition" href="?campus=northshore">North Shore</Link>
+        <Link className="hover:underline transition" href="?campus=squamish">Squamish</Link>
+        <Link className="hover:underline transition" href="?campus=toronto">Toronto</Link>
       </div>
     </section>
   );
