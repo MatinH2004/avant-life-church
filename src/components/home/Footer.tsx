730cb4335cf49004646269fadcc7c206fc624a18
Matin Hassan Pour <matinhp04@gmail.com>
2025-10-02 15:09:25 -0700

    fix centralized footer data

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,46 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
-
-const footerLinks = [
-  {
-    title: "LOCATIONS",
-    links: [
-      { href: "/", label: "North Shore" },
-      { href: "/", label: "Squamish" },
-      { href: "/", label: "Toronto" },
-    ],
-  },
-  {
-    title: "ABOUT US",
-    links: [
-      { href: "/", label: "Our Team" },
-      { href: "/", label: "Our Story" },
-      { href: "/", label: "We Believe" },
-      { href: "/", label: "About Jesus" },
-    ],
-  },
-  {
-    title: "GET CONNECTED",
-    links: [
-      { href: "/", label: "Praise & Prayer" },
-      { href: "/", label: "Join a Life Group" },
-      { href: "/", label: "Find a church" },
-      { href: "/", label: "Give Online" },
-      { href: "/", label: "21 Days Devotional", },
-      { href: "mailto:info@avantlifechurch.com", label: "Inquiries" },
-    ],
-  },
-];
+import { footerData } from "@/data/footerData";
 
 export default function Footer() {
   return (
     <footer className="flex flex-col md:flex-row justify-between py-8 px-8 mt-8 bg-black text-gray-100 rounded-t-[50px]">
       {/* Render sections dynamically */}
-      {footerLinks.map((section) => (
+      {footerData.map((section) => (
         <div key={section.title} className="mb-6 md:mb-0">
           <h3 className="text-xl mb-2">{section.title}</h3>
           <ul>
-            {section.links.map((link) => (
+            {section.items.map((link) => (
               <li key={link.label}>
                 <Link
                   href={link.href}
