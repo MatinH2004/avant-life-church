d258088ea7b62e9fbd944d47bf12dc4303a7be1b
Matin Hassan Pour <matinhp04@gmail.com>
2025-10-14 23:24:53 -0700

    feat about page text

--- src/components/home/about/AboutText.tsx
+++ src/components/home/about/AboutText.tsx
@@ -1,31 +1,140 @@
+interface Sections {
+  title: string;
+  paragraphs: string[];
+}
+
 export default function AboutText() {
+  const sections: Sections[] = [
+    {
+      title: "Our Mission",
+      paragraphs: [
+        `We&apos;re here to impact the world by starting right here in Canada, building a Church that&apos;s alive, bold, and centred on Jesus. A Church where the Bible is our foundation, but our expression is creative, innovative, and life-giving. We believe God has called us to shift mindsets, raise leaders, and empower people to influence culture in every sphere of life. Because God&apos;s not finished and neither are we.`,
+        `Avant Life Church was born from this conviction: to go first, to be original, to be a movement that carries the ancient message of Jesus in fresh, powerful ways. We&apos;re a Church that doesn&apos;t just keep up with culture, we want to help shape it. Through worship, creativity, and community, we&apos;re making space for people of all generations to encounter Jesus and step into the future He has for them.`,
+        `<b>Isaiah 43:19 says:</b>`,
+        `<i>“See, I am doing a new thing! Now it springs up; do you not perceive it? I am making a way in the wilderness and streams in the wasteland.”</i>`
+      ],
+    },
+    {
+      title: "The Church We See",
+      paragraphs: [
+        `The Church we see is unstoppable - so passionate about bringing the good news of Jesus to the world that it can&apos;t be ignored by a city or a nation. It&apos;s a Church where auditoriums overflow with people of every culture, background, and generation responding to Christ&apos;s call to salvation.`,
+        `We see a Church whose worship shakes the atmosphere, authentic, passionate, and powerful enough to touch Heaven and change the earth. A sound that influences worshippers across the globe, lifting high the name of Jesus with declarations of faith, hope, and victory.`,
+        `We see a Church that isn&apos;t afraid to innovate, using creativity and new technologies to share Christ in ways that connect with every generation and demographic. A Church that is multi-ethnic, multi-location, multi-expression, and multi-language - a global family united under one name, Jesus.`,
+        `We see a Church that transforms its city while reaching far beyond its borders. A Church that thousands call home and countless more look to as a beacon of hope. Fueled by Christ&apos;s love and empowered by the Holy Spirit, we are a movement that believes anything is possible.`,
+        `This is the Church we see!`,
+      ],
+    },
+    {
+      title: "What to Expect",
+      paragraphs: [
+        `<b>1. Sunday Expression</b>`,
+        `People today are searching for a faith that is both deeply spiritual and meaningfully connected to their everyday lives. That&apos;s why our Sunday gatherings are designed to be practical, relevant, and spiritually empowering. Each week, our heart is to create an atmosphere where people encounter God in a way that leaves them restored, enriched, motivated, and empowered through dynamic, contemporary worship. Beyond the main gathering, Sundays also offer opportunities for every generation to grow through our thriving Kids Life ministry, transformative Journey sessions, and diverse Academy studies that equip people at every stage of life.`,
+        `<b>2. Community Engagement</b>`,
+        `One of our greatest desires as a Church is to be known for our love for the community and the fellowship we cultivate. At Avant Life Church, we are intentional about creating pathways that connect individuals, youth, and families into the life of the Church. Whether through a community event hosted at Avant Life Church, an educational program in our Early Learning Centre, or through outreach initiatives that serve the practical needs of our city, we are committed to being the hands and feet of Jesus. We strive to not only welcome people into our gatherings but also to actively go out and serve the community we reside in, meeting people where they are and demonstrating Christ&apos;s love in tangible ways.`,
+        `<b>3. Discipleship & Leadership Development</b>`,
+        `We believe Sunday is the launchpad, not the finish line. What happens in Church should spill into every part of our week, our campuses, workplaces, schools, coffee shops, and homes. At Avant Life Church, our heart is to equip people to live boldly for Jesus wherever they are, becoming leaders who shape culture and pioneer new ways of sharing God&apos;s Word. We&apos;re committed to raising and releasing a generation of leaders who dream big, take risks for the Kingdom, and step fully into all that God has called them to. By investing in the next generation, we&apos;re building a movement that impacts neighbourhoods, campuses, and communities across nations. Developing leaders isn&apos;t just a program for us—it&apos;s the heartbeat of how we see God&apos;s vision expand and transform the world for His glory.`,
+        `<b>4. Inspirational Creativity</b>`,
+        `Creativity is one of the most powerful ways we can expand the Kingdom. At Avant Life Church, we believe every creative gift whether it&apos;s music, design, film, fashion, writing, or digital media has the power to carry the Gospel into spaces the Church has never reached before. Our dream is to be a creative hub where people can use their skills not just in worship, but also in bold, boundary-pushing ways that inspire culture and spark conversations about Jesus. God has always used creative voices to build His Kingdom, and He&apos;s still doing it today. Our responsibility is to create space, empower talent, and release creatives to shine light into the world impacting more lives, more communities, and more nations than we could ever imagine.`,
+        `<b>5. Growth</b>`,
+        `We are a Church that plants Churches - because the mission of Jesus has always been multiplication, not maintenance. The Great Commission calls us to go, reach, and build, and we believe the best way to reach the lost is through bold expansion.`,
+        `From the beginning, God placed growth and multiplication in the DNA of Avant Life Church. That&apos;s why our launch in Toronto isn&apos;t just another step - it&apos;s a defining moment. In one of the most influential cities in the world, we are planting a Church that will raise leaders, impact culture, and create new opportunities for people to encounter Jesus.`,
+        `Every time we plant, the Kingdom expands, communities are renewed, and the Gospel reaches further than before. For Avant Life Church, planting isn&apos;t just part of the vision it&apos;s the heartbeat of the vision. Toronto is only the beginning of greater things ahead.`,
+      ],
+    },
+    {
+      title: "Our Belief",
+      paragraphs: [
+        `<b>We believe</b> that the Bible is God's Word. It is accurate, authoritative and applicable to our everyday lives.`,
+        `<b>We believe</b> in one eternal God who is the Creator of all things. He exists in three Persons: God the Father, God the Son and God the Holy Spirit. He is totally loving and completely holy.`,
+        `<b>We believe</b> that sin has separated each of us from God and His purpose for our lives.`,
+        `<b>We believe</b> that the Lord Jesus Christ as both God and man is the only One who can reconcile us to God. He lived a sinless and exemplary life, died on the cross in our place, and rose again to prove His victory and empower us for life.`,
+        `<b>We believe</b> that in order to receive forgiveness and the 'new birth' we must repent of our sins, believe in the Lord Jesus Christ, and submit to His will for our lives.`,
+        `<b>We believe</b> that in order to live the holy and fruitful lives that God intends for us, we need to be baptised in water and be filled with the power of the Holy Spirit. The Holy Spirit enables us to use spiritual gifts, including speaking in tongues.`,
+        `<b>We believe</b> in the power and significance of the Church and the necessity of believers to meet regularly together for fellowship, prayer and the 'breaking of bread'.`,
+        `<b>We believe</b> that God has individually equipped us so that we can successfully achieve His purpose for our lives which is to worship God, fulfill our role in the Church and serve the community in which we live.`,
+        `<b>We believe</b> that God wants to heal and transform us so that we can live healthy and blessed lives in order to help others more effectively.`,
+        `<b>We believe</b> that our eternal destination of either Heaven or hell is determined by our response to the Lord Jesus Christ.`,
+        `<b>We believe</b> that the Lord Jesus Christ is coming back again as He promised.`
+      ],
+    },
+    {
+      title: "A Culture of Serving",
+      paragraphs: [
+        `At Avant Life Church, we believe serving is at the heart of who we are. Jesus showed us that true greatness comes through serving others, and we are passionate about creating a culture where everyone has the opportunity to make a difference. Serving isn&apos;t just about filling a role, it&apos;s about carrying the love of Christ into every interaction and experience. On Sundays, this comes to life through our three main areas: Hosting, where we create a warm and welcoming environment for every person who walks through the doors; Kids Life, where we invest in the next generation and teach them to know and love Jesus; and Creative Arts, where we use music, media, and production to lead people into powerful encounters with God. Every act of service carries impact, and together our serving culture transforms lives, builds the Church, and advances the Kingdom.`,
+        `There&apos;s a place for you on the team. Jump in, use your gifts, and be part of the impact God is making through Avant Life Church.`
+      ],
+    },
+    {
+      title: "Our Governance",
+      paragraphs: [
+        `Avant Life Church is led with both vision and accountability. Our Lead Pastors provide spiritual direction and overall leadership, guiding the Church in its mission and calling. They are supported by a Board of Directors, who oversee governance, stewardship, and strategic decision-making. In addition, our voting membership is made up of our pastors from all campuses, ensuring that voices from across the Church are represented. This structure allows us to remain Spirit-led, mission-focused, and steward every opportunity with wisdom and integrity.re statement`,
+      ],
+    },
+    {
+      title: "Created to Worship",
+      paragraphs: [
+        `Worship is the wholehearted response of God&apos;s people to who He is and what He has done. This is expressed in love, reverence, and obedience through prayer, praise, song, service, and daily living, according to His Word.`,
+        `<b>Collective Worship</b>`,
+        `When we gather, our worship isn&apos;t random. It&apos;s rooted in the Bible. Scripture shows us different Hebrew words for how God&apos;s people have always expressed praise. Each one unlocks a new layer of what it means to give God everything.`,
+        `<b>Halal (הָלַל)</b> → To boast, rave, and go all out, like celebrating so big it looks crazy to the world. (Yes, that&apos;s where Hallelujah comes from.)`,
+        `<b>Yadah (יָדָה)</b> → Throwing up your hands in gratitude and surrender. A physical way of saying, “God, it&apos;s Yours.”`,
+        `<b>Todah (תּוֹדָה)</b> → Thanking God before you see the answer. Lifting your hands as a faith move.`,
+        `<b>Barak (בָּרַךְ)</b> → Bowing low, kneeling in reverence. A quiet but powerful posture of respect.`,
+        `<b>Tehillah (תְּהִלָּה)</b> → Singing out praise—especially those raw, unscripted songs that come straight from the Spirit.`,
+        `<b>Zamar (זָמַר)</b> → Using instruments to make music that glorifies Him. (Yes, drums, guitars, beats—He loves it all.)`,
+        `<b>Shabach (שָׁבַח)</b> → Shouting out loud with joy and victory. Giving God the same energy you&apos;d give your favorite team in overtime.`,
+        `We believe worship is meant to be alive, expressive, and full-body. That&apos;s why we sing, play, shout, and even dance because when the church comes together as family, God deserves our whole selves in praise.`,
+      ],
+    },
+    {
+      title: "Women in Leadership",
+      paragraphs: [
+        `The question of how men and women relate and engage in church leadership offices (particularly pastor and elder) has been a subject of significant debate at various points in church history. There are many considerations within the broader subject of men and women in leadership that are dealt with in the Bible. Here we have included a summary of things we see taught across the arc of Scripture that directly bear on our church’s position and practice.`,
+        `We believe that full equality between women and men does not mean women and men are completely the same. There is goodness and beauty uniquely represented in each gender that profoundly reflects and glorifies God. `,
+        `We believe God gives leadership in the church on the basis of grace, calling, spiritual gifts, obedience, and character.`,
+        `We believe both women and men can and should lead, preach, pastor, and minister within the church. We believe women and men doing this together will result in a more robust ministry of love and grace than one gender can sustain alone.`,
+        `We believe women and men can and should serve in pastoral leadership in the local church.`,
+        `Avant Life Church believes that men and women are equally gifted and qualified to lead and serve as co-laborers in the church. We do not simply permit, but expressly value, the presence of both men and women in these various facets of church leadership, including the role of elder.`,
+      ],
+    },
+  ];
+
   return (
-    <section>
-      <h2 className="text-center text-3xl font-semibold">STATEMENT OF FAITH.</h2>
-      <div className="mx-auto px-6 py-8 max-w-screen-sm flex justify-center">
-        <p>
-          <b>We believe</b> that the Bible is God&apos;s Word. It is accurate, authoritative and applicable to our everyday lives.
-          <br /><br />
-          <b>We believe</b> in one eternal God who is the Creator of all things. He exists in three Persons: God the Father, God the Son and God the Holy Spirit. He is totally loving and completely holy.
-          <br /><br />
-          <b>We believe</b> that sin has separated each of us from God and His purpose for our lives.
-          <br /><br />
-          <b>We believe</b> that the Lord Jesus Christ as both God and man is the only One who can reconcile us to God. He lived a sinless and exemplary life, died on the cross in our place, and rose again to prove His victory and empower us for life.
-          <br /><br />
-          <b>We believe</b> that in order to receive forgiveness and the &apos;new birth&apos; we must repent of our sins, believe in the Lord Jesus Christ, and submit to His will for our lives.
-          <br /><br />
-          <b>We believe</b> that in order to live the holy and fruitful lives that God intends for us, we need to be baptised in water and be filled with the power of the Holy Spirit. The Holy Spirit enables us to use spiritual gifts, including speaking in tongues.
-          <br /><br />
-          <b>We believe</b> in the power and significance of the Church and the necessity of believers to meet regularly together for fellowship, prayer and the &apos;breaking of bread&apos;.
-          <br /><br />
-          <b>We believe</b> that God has individually equipped us so that we can successfully achieve His purpose for our lives which is to worship God, fulfill our role in the Church and serve the community in which we live.
-          <br /><br />
-          <b>We believe</b> that God wants to heal and transform us so that we can live healthy and blessed lives in order to help others more effectively.
-          <br /><br />
-          <b>We believe</b> that our eternal destination of either Heaven or hell is determined by our response to the Lord Jesus Christ.
-          <br /><br />
-          <b>We believe</b> that the Lord Jesus Christ is coming back again as He promised.
-        </p>
+    <>
+      {sections.map((section, index) => {
+        // Create a URL-safe id from the title
+        const id = section.title
+          .toLowerCase()
+          .replace(/[^\w\s-]/g, "") // remove punctuation
+          .replace(/\s+/g, "-"); // replace spaces with hyphens
+
+        return (
+          <ContentSection
+            key={index}
+            id={id}
+            title={section.title}
+            paragraphs={section.paragraphs}
+          />
+        );
+      })}
+    </>
+  );
+}
+
+interface ContentSectionProps extends Sections {
+  id: string;
+}
+
+function ContentSection({ id, title, paragraphs }: ContentSectionProps) {
+  return (
+    <section id={id} className="py-8 scroll-mt-24">
+      <h2 className="text-center text-3xl font-semibold mb-4">{title}</h2>
+      <div className="mx-auto px-6 max-w-screen-sm flex justify-center">
+        <div className="space-y-4 text-justify">
+          {paragraphs.map((text, i) => (
+            <p key={i} dangerouslySetInnerHTML={{ __html: text }} />
+          ))}
+        </div>
       </div>
     </section>
   );
