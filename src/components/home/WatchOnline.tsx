300764d5053136dbaf3581214faae7ab1108bc8e
Matin Hassan Pour <matinhp04@gmail.com>
2025-11-04 17:50:35 -0800

    fix btn order & xl screen spacing

--- src/components/home/WatchOnline.tsx
+++ src/components/home/WatchOnline.tsx
@@ -6,7 +6,7 @@ export default function WatchOnline() {
     <section className="py-8">
       <div className="flex flex-col md:flex-row items-center md:items-start">
         {/* Text + Buttons */}
-        <div className="px-12 md:pl-18 max-w-xl md:max-w-2xl mx-auto md:mx-0 md:flex-1">
+        <div className="px-12 md:ml-18 xl:ml-42 max-w-xl md:max-w-2xl mx-auto md:mx-0 md:flex-1">
           <h2 className="text-4xl md:text-5xl font-semibold">
             Join us from anywhere around the world
           </h2>
@@ -15,8 +15,8 @@ export default function WatchOnline() {
             Missed a week? Watch past sermons and learn more about our ministries on our YouTube channel.
           </p>
           <div className="flex flex-col items-start py-4">
-            <Button href="https://www.youtube.com/c/AvantLifeChurch" text="YOUTUBE CHANNEL" bg="red" bold={false} />
             <Button href="https://avantlifechurch.online.church" text="WATCH LIVE" bg="blue" bold={false} />
+            <Button href="https://www.youtube.com/c/AvantLifeChurch" text="YOUTUBE CHANNEL" bg="red" bold={false} />
           </div>
         </div>
 
