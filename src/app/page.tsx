fa004565aa58aefce82afe8026cf4307e3cdaef2
Matin Hassan Pour <matinhp04@gmail.com>
2025-11-18 11:26:21 -0800

    delete carousel from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/home/Hero";
-import Carousel from "@/components/ui/Carousel";
 import Locations from "@/components/home/Locations";
 import WatchOnline from "@/components/home/WatchOnline";
 import Team from "@/components/home/Team";
@@ -8,7 +7,6 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      <Carousel />
       <Locations />
       <WatchOnline />
       <Team />
