31357053212a8625339df722cf3039d56aac95a7
Matin Hassan Pour <matinhp04@gmail.com>
2025-12-16 20:41:24 -0800

    update giving components location

--- src/app/give/page.tsx
+++ src/app/give/page.tsx
@@ -1,6 +1,6 @@
-import GivingDetails from "@/components/give/GivingDetails";
-import GivingMethods from "@/components/give/GivingMethods";
-import Hero from "@/components/give/Hero";
+import GivingDetails from "@/components/home/give/GivingDetails";
+import GivingMethods from "@/components/home/give/GivingMethods";
+import Hero from "@/components/home/give/Hero";
 
 export default function GivingPage() {
   return (
